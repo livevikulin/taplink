3e83a769d58e9d2afbb1fa921098cd2fc1620279
Виталя Викулин <livevikulin@gmail.com>
2020-10-13 13:35:20 +0700

    finish

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
 	$('.slider').slick({
 		arrows: false,
-		dots: true
+		dots: true,
+		responsive: [
+			{
+			  breakpoint: 768,
+			  settings: {
+				centerMode: true,
+				centerPadding: '8px'
+			  }
+			}
+		  ]
 	});
 });
